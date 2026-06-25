312be292ccb522a6db13e45382c50b6ba5a8fedc
jason13official <jason13official@gmail.com>
2026-06-25 13:07:14 -0400

    hurt shield when blocking

--- common/src/main/java/io/github/jason13official/new_shield_variants/impl/common/registry/ModItems.java
+++ common/src/main/java/io/github/jason13official/new_shield_variants/impl/common/registry/ModItems.java
@@ -66,7 +66,7 @@ public class ModItems {
             0.25F,
             1.0F,
             List.of(new BlocksAttacks.DamageReduction(90.0F, Optional.empty(), 0.0F, 1.0F)),
-            new BlocksAttacks.ItemDamageFunction(3.0F, 1.0F, 1.0F),
+            new BlocksAttacks.ItemDamageFunction(1.0F, 1.0F, 0.0F),
             Optional.of(context.getOrThrow(DamageTypeTags.BYPASSES_SHIELD)),
             Optional.of(SoundEvents.SHIELD_BLOCK),
             Optional.of(SoundEvents.SHIELD_BREAK)));
